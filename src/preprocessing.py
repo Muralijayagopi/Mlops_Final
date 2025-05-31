4b26739f380f2124f6a25cc2a33fabd734c0a070
Muralijayagopi <jmurali.krishnann@gmail.com>
2025-05-31 01:43:04 +0000

    updated couple of files

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,8 +1,8 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-def load_and_split_data(file_path):
-    df = pd.read_csv(file_path)
+def load_and_split_data(csv_path='data/salary_data.csv'):
+    df = pd.read_csv(csv_path)
     X = df[['YearsExperience']]
     y = df['Salary']
     return train_test_split(X, y, test_size=0.2, random_state=42)
--- src/train.py
+++ src/train.py
@@ -3,11 +3,11 @@ from sklearn.linear_model import LinearRegression
 from src.preprocessing import load_and_split_data
 
 def train_model():
-    X_train, X_test, y_train, y_test = load_and_split_data('data/salary_data.csv')
+    X_train, _, y_train, _ = load_and_split_data('data/salary_data.csv')
     model = LinearRegression()
     model.fit(X_train, y_train)
     joblib.dump(model, 'src/model.pkl')
-    print("Model trained and saved as 'model.pkl'.")
+    print("✅ Model trained and saved as 'src/model.pkl'.")
 
 if __name__ == '__main__':
     train_model()
